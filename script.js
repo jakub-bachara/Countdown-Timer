40a21a00e4f4207b8492e69b4e8a6c57d93a9c93
jakub-bachara <jakub.bachara@gmail.com>
2020-07-17 23:11:38 +0200

    użytkownik wybiera date + wyjście z dublowania interwału

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 function main(){
-var countDownDate=new Date("Jan 11, 2020 09:00:00").getTime();
+var date2=document.getElementById("date").value;
+var countDownDate=new Date(date2+ 'T00:00').getTime();
 var dni=document.getElementById("dni");
 var godziny=document.getElementById("godziny");
 var minuty=document.getElementById("minuty");
 var sekundy=document.getElementById("sekundy");
-
-var countDown=setInterval(function(){
+var countDown=setInterval(() =>{
 var now=new Date().getTime();
 var distance=countDownDate-now;
 var days=Math.floor(distance/(1000*60*60*24));
 var hours = Math.floor(distance % (1000*60*60*24)/(1000*60*60));
 var minutes = Math.floor(distance % (1000*60*60)/(1000*60));
 var seconds = Math.floor(distance % (1000*60)/(1000));
-if(distance<0){
-    clearInterval(countDown);
+if(seconds>20){
+    clearInterval(1);
     alert("DATA JUŻ MINĘŁA!");
 }
 else{
+    console.log(countDown);
 dni.innerHTML=days+" D";
 godziny.innerHTML=hours+" H";
 minuty.innerHTML=minutes+" M";
